26575911047a106c93423a8ebddbab9b4890f1c9
Marie-Line <Marie-Line>
2017-06-05 11:44:04 +0200

    rename the DGEList object into "obj"

--- trim_late_time_points.R
+++ trim_late_time_points.R
@@ -1,8 +1,9 @@
-trim_late_time_points <- function(DGeList =obj, CPMcutoff= 0.5){
+trim_late_time_points <- function(DGEList =obj, CPMcutoff= 0.5){
   #replaces late time points with cpm lower than threshold with NA
   #replaces all time points later than a NA time point with NA
   #if there is a cpm increase of more than 20 % between two time points, turn into NA
   #stores the "trimmed cpm" data in obj$trimmed
+  obj <- DGEList
   q <- ifelse(cpm(obj)<CPMcutoff,NA,cpm(obj))
   q <- apply(q,1, function(x){
     for(i in 1:(length(x)-1)){
