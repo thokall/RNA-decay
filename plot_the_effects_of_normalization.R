8265dcbac3f66dec7d391349974f1dcb46624c52
Marie-Line <Marie-Line>
2017-06-05 11:43:20 +0200

    use already normalized data to make the plots

--- plot_the_effects_of_normalization.R
+++ plot_the_effects_of_normalization.R
@@ -16,11 +16,13 @@ plot_the_effects_of_normalization <- function(DGEList = obj, specific.genes = c(
   }
   
   par(mfrow = c(length(genes),2))
-  data <- obj$cpm_trimmed_normalized
+  data <- obj$cpm_trimmed
   data  <- apply(data,2,as.numeric)
+  norm_data <- obj$cpm_trimmed_normalized
+  norm_data  <- apply(norm_data,2,as.numeric)
  
   ##create a time point vector that fits the main data
-  t <- sapply(strsplit(colnames(log2data),"t"), function(x){x[!x ==""]})
+  t <- sapply(strsplit(colnames(data),"t"), function(x){x[!x ==""]})
   t <- as.numeric(t)
   
   for(i in 1:length(genes)){
@@ -28,7 +30,7 @@ plot_the_effects_of_normalization <- function(DGEList = obj, specific.genes = c(
     #adjust the time point vector to the specific gene
     realt <- suppressWarnings(t+data[i,]-data[i,])
     plot(realt,data[genes[i],], type = "b", main = paste("raw data:",obj$genes$symbol[genes[i]]), log = "y", xlab = "time")
-    plot(realt,data[genes[i],]*obj$samples$norm.factors, type= "b", main = paste("normalized:",obj$genes$symbol[genes[i]]), log = "y", xlab = "time")
+    plot(realt,norm_data[genes[i],], type= "b", main = paste("normalized:",obj$genes$symbol[genes[i]]), log = "y", xlab = "time")
     
     
   
